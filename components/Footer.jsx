8503b3024229d0e67e45eacb531115324ab27f8b
nesssrina2019 <nesssrina2019@gmail.com>
2022-10-01 17:48:50 +0100

    change title

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 Computers All rights reserved</p>
+      <p>2022 Computer All rights reserved</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
